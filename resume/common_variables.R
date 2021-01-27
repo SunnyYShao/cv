3ac0afabe4b58843cd28436c5c7ad905352fb918
SunnyYShao <yshao006@ucr.edu>
2021-01-26 16:21:43 -0800

    remove duplicated files

--- resume/common_variables.R
+++ resume/common_variables.R
@@ -1,14 +1,17 @@
 # Common non-csv data between CV and resume
 
 # Header with description of what is trying to be accomplished with this CV/Resume
-intro_text <- "I have made [visualizations viewed tens of thousands of people](https://censusmaps.aapidata.com), led data queries, analysis, and visualization labs for UCR students and members from [public and non-profit sectors](https://sunnyshao.com/#clients)."
+intro_text <- "I am a PhD Candiate in political science focuses on social media narrative and 
+political influence. My job as the senior associate research for both AAPI Data and CSI UCR 
+gave me over four years of experiences in research and data science. I have made [data visualization tools viewed tens of thousands of people]
+(https://censusmaps.aapidata.com), led survey design, data collection, analysis, 
+and visualization projects for both [public and private stakeholders](https://sunnyshao.com/#clients)."
 
 
 # Language skills for skill bars visualization
 skills <- tribble(
   ~skill,               ~level,
   "R",                  5,
-  "STATA ",             5,
   "Python",             4,
   "SQL",                4,
   "CSS/HTML",           3
